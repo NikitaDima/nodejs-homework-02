db443efd8507245e8199787d3fccb3b8117ced5b
NikitaDima <onekill13@gmail.com>
2023-07-06 06:31:44 +0300

    Відправка ментору

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,8 @@
 const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
+const hendleMongooseError = require('./hendleMongooseError');
 module.exports = {
   HttpError,
   ctrlWrapper,
+  hendleMongooseError,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const validateBody = require('../../middlewares/validateBody');
-const schemas = require('../../schemas/contactsSchemas');
+const { validateBody, isValidId } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 const controlles = require('../../controllers/contacts');
 
 router.get('/', controlles.getAll);
 
-router.get('/:contactId', controlles.getById);
+router.get('/:contactId', isValidId, controlles.getById);
 
 router.post('/', validateBody(schemas.addSchema), controlles.addContact);
 
-router.delete('/:contactId', controlles.deleteContact);
+router.delete('/:contactId', isValidId, controlles.deleteContact);
 
 router.put(
   '/:contactId',
+  isValidId,
   validateBody(schemas.addSchema),
   controlles.updateById
+);
+
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  controlles.updateFavorite
 );
 
 module.exports = router;
